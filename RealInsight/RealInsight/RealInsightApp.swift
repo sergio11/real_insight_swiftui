8916cc245517c6fc40c94403d69f6090f63561e6
Sergio Sánchez <sss4esob@gmail.com>
2024-02-04 12:32:22 +0100

    Initial commit

---
+++ RealInsight/RealInsight/RealInsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealInsightApp.swift
+//  RealInsight
+//
+//  Created by Sergio Sánchez Sánchez on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RealInsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
