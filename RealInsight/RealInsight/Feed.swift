e3264bf6f49f66a4191168b30447daf954a4a83d
Sergio Sánchez <sss4esob@gmail.com>
2024-02-04 13:10:08 +0100

    Implement Feed scroll view

--- RealInsight/RealInsight/Feed.swift
+++ RealInsight/RealInsight/Feed.swift
@@ -11,39 +11,95 @@ struct Feed: View {
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
+            
             ZStack {
-                VStack {
+                
+                ScrollView {
                     VStack {
-                        HStack {
-                            Image(systemName: "person.2.fill")
-                                .foregroundColor(.white)
-                            Spacer()
-                            Text("RealInsight.")
-                                .foregroundColor(.white)
-                                .fontWeight(.bold)
-                                .font(.system(size: 22))
-                            Spacer()
-                            Image("example")
-                                .resizable()
-                                .frame(width: 35, height: 35)
-                                .cornerRadius(17.5)
+                        VStack {
+                            ZStack {
+                                VStack(alignment: .leading) {
+                                    Image("example")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .cornerRadius(5)
+                                }
+                                
+                                VStack {
+                                    HStack {
+                                        Image("example")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .border(.black)
+                                            .cornerRadius(2)
+                                            .frame(width: 20, height: 40)
+                                            .padding(.leading)
+                                        Spacer()
+                                    }
+                                    .padding(.top, 18)
+                                    Spacer()
+                                }
+                                
+                                
+                            }.frame(width: 100)
+                            
                         }
-                        .padding(.horizontal)
                         
-                        HStack {
-                            Text("My friends")
+                        VStack {
+                            Text("Add a caption...")
                                 .foregroundColor(.white)
                                 .fontWeight(.semibold)
-                            
-                            Text("Discovery")
+                            Text("View comment")
                                 .foregroundColor(.gray)
-                    
+                            
+                            HStack {
+                                Text("Location * 1 hr late")
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 12))
+                                ThreeDots(size: 3, color: .gray)
+                            }
                         }
                         
+                        ForEach(1..<8) { _ in
+                            FeedCell()
+                        }
+                        
+                    }.padding(.top, 80)
+                    
+                }
+            }
+            
+            VStack {
+                VStack {
+                    HStack {
+                        Image(systemName: "person.2.fill")
+                            .foregroundColor(.white)
                         Spacer()
-                        
-                        
+                        Text("RealInsight.")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                            .font(.system(size: 22))
+                        Spacer()
+                        Image("example")
+                            .resizable()
+                            .frame(width: 35, height: 35)
+                            .cornerRadius(17.5)
                     }
+                    .padding(.horizontal)
+                    
+                    HStack {
+                        Text("My friends")
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                        
+                        Text("Discovery")
+                            .foregroundColor(.gray)
+                
+                    }
+                    
+                    Spacer()
+                    
+                    
                 }
             }
         }
