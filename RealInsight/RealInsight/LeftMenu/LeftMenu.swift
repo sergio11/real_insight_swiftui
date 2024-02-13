29bf84949c87f8f34896c03f209882870c879c9c
Sergio Sánchez <sss4esob@gmail.com>
2024-02-13 20:12:39 +0100

    Implement LeftMenu tabs

--- RealInsight/RealInsight/LeftMenu/LeftMenu.swift
+++ RealInsight/RealInsight/LeftMenu/LeftMenu.swift
@@ -8,12 +8,83 @@
 import SwiftUI
 
 struct LeftMenu: View {
+    
+    @State var width = UIScreen.main.bounds.width
+    @State var menu = "suggestions"
+    
     var body: some View {
         VStack {
             ZStack {
                 Color.black.ignoresSafeArea()
                 LeftMenuTopView()
-                Suggestions()
+                
+                if menu == "suggestions" {
+                    Suggestions()
+                } else if menu == "friends" {
+                    FriendsView()
+                } else if menu == "requests" {
+                    RequestsView()
+                }
+                
+                VStack {
+                    
+                    Spacer()
+                    
+                    ZStack {
+                        VStack {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 22)
+                                    .frame(width: width * 0.7, height: 40)
+                                    .foregroundColor(Color(red: 20/255, green: 20/255, blue: 20/255))
+                                
+                                HStack(spacing: 4) {
+                                    Capsule()
+                                        .frame(width: 100, height: 28)
+                                        .foregroundColor(Color(red: 46/255, green: 46/255, blue: 48/255))
+                                        .opacity(menu == "suggestions" ? 1 : 0)
+                                        .overlay(
+                                            Text("Suggestions")
+                                                .foregroundColor(.white)
+                                                .font(.system(size: 14))
+                                        ).onTapGesture {
+                                            menu = "suggestions"
+                                        }
+                                    
+                                    
+                                    Capsule()
+                                        .frame(width: 70, height: 28)
+                                        .foregroundColor(Color(red: 46/255, green: 46/255, blue: 48/255))
+                                        .opacity(menu == "friends" ? 1 : 0)
+                                        .overlay(
+                                            Text("Friends")
+                                                .foregroundColor(.white)
+                                                .font(.system(size: 14))
+                                        ).onTapGesture {
+                                            menu = "friends"
+                                        }
+                                    
+                                    Capsule()
+                                        .frame(width: 75, height: 28)
+                                        .foregroundColor(Color(red: 46/255, green: 46/255, blue: 48/255))
+                                        .opacity(menu == "requests" ? 1 : 0)
+                                        .overlay(
+                                            Text("Requests")
+                                                .foregroundColor(.white)
+                                                .font(.system(size: 14))
+                                        ).onTapGesture {
+                                            menu = "requests"
+                                        }
+                                }
+                            }
+                        }
+                        .zIndex(1)
+                        
+                        LinearGradient(colors: [.black, .white.opacity(0)], startPoint: .bottom, endPoint: .top)
+                            .ignoresSafeArea()
+                            .frame(height: 60)
+                            .opacity(0.9)
+                    }
+                }
             }
         }
     }
