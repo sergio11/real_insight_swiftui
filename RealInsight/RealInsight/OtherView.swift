d32ecd887cfdceb16edf4ef4f71d25240f1753e9
Sergio Sánchez <sss4esob@gmail.com>
2024-02-13 20:44:10 +0100

    Implement Others view

---
+++ RealInsight/RealInsight/OtherView.swift
@@ -0,0 +1,121 @@
+//
+//  OtherView.swift
+//  RealInsight
+//
+//  Created by Sergio Sánchez Sánchez on 13/2/24.
+//
+
+import SwiftUI
+
+struct OtherView: View {
+    
+    @State var fastCamera = false
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Color.black.ignoresSafeArea()
+                
+                VStack {
+                    ZStack {
+                        Text("Other")
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                        
+                        HStack {
+                            Image(systemName: "arrow.backward")
+                                .foregroundColor(.white)
+                                .font(.system(size: 20))
+                            
+                            Spacer()
+                        }
+                    }
+                    Spacer()
+                }
+                
+                VStack {
+                    VStack(spacing: 14) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(height: 45)
+                                .foregroundColor(Color(red: 28/255, green: 28/255, blue: 30/255))
+                            
+                            HStack {
+                                Image(systemName: "camera.viewfinder")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 18))
+                                
+                                Text("Fast camera (reduces quality)")
+                                    .foregroundColor(.white)
+                                    .fontWeight(.medium)
+                                    .font(.system(size: 14))
+                                
+                                Toggle("", isOn: $fastCamera)
+                            }
+                            .padding(.horizontal, UIScreen.main.bounds.width * 0.04)
+                            .frame(height: 30)
+                            
+                        }
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(height: 45)
+                                .foregroundColor(Color(red: 28/255, green: 28/255, blue: 30/255))
+                            
+                            HStack {
+                                Image(systemName: "xmark.app")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 18))
+                                
+                                Text("Clear cache")
+                                    .foregroundColor(.white)
+                                    .fontWeight(.medium)
+                                    .font(.system(size: 14))
+                                
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 14))
+                            }
+                            .padding(.horizontal, UIScreen.main.bounds.width * 0.04)
+                            .frame(height: 30)
+                            
+                        }
+                        
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(height: 45)
+                                .foregroundColor(Color(red: 28/255, green: 28/255, blue: 30/255))
+                            
+                            HStack {
+                                Spacer()
+                                
+                                Text("Delete account")
+                                    .foregroundColor(.red)
+                                
+                                Spacer()
+                            
+                            }
+                            .padding(.horizontal, UIScreen.main.bounds.width * 0.04)
+                            .frame(height: 30)
+                            
+                        }
+                        
+                    }
+                    .padding(.horizontal)
+                    .padding(.top, 50)
+                    
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+struct OtherView_Previews: PreviewProvider {
+    static var previews: some View {
+        OtherView()
+    }
+}
