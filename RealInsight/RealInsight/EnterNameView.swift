6fb2288a2be2d64a01c4787b3bbd40109963b595
Sergio Sánchez <sss4esob@gmail.com>
2024-02-26 18:44:00 +0100

    Implement EnterNameView

---
+++ RealInsight/RealInsight/EnterNameView.swift
@@ -0,0 +1,68 @@
+//
+//  EnterNameView.swift
+//  RealInsight
+//
+//  Created by Sergio Sánchez Sánchez on 26/2/24.
+//
+
+import SwiftUI
+
+struct EnterNameView: View {
+    
+    @State var name: String = ""
+    @State var buttonActive: Bool = false
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Color.black.ignoresSafeArea()
+                VStack {
+                    HStack {
+                        Spacer()
+                        Text("RealInsight.")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                            .font(.system(size: 22))
+                        Spacer()
+                    }
+                    Spacer()
+                }
+                VStack {
+                    VStack {
+                        Text("Let's get started, what's your name?")
+                            .fontWeight(.heavy)
+                            .font(.system(size: 16))
+                        
+                        Text(name.isEmpty ? "Your name": "")
+                            .foregroundColor(name.isEmpty ? Color(red: 70/255, green: 70/255, blue: 73/255): Color.black)
+                            .fontWeight(.heavy)
+                            .font(.system(size: 40))
+                            .frame(width: 210)
+                            .overlay(
+                                TextField("", text: $name)
+                                    .font(.system(size: 40, weight: .heavy))
+                            )
+                            .padding(.top, 5)
+                    }
+                    .foregroundColor(.white)
+                    Spacer()
+                }
+                .padding(.top, 50)
+                
+                VStack {
+                    Spacer()
+                    WhiteButtonView(buttonActive: $buttonActive, text: "Continue")
+                        .onChange(of: name) { newValue in
+                            buttonActive = !newValue.isEmpty
+                        }
+                }
+            }
+        }
+    }
+}
+
+struct EnterNameView_Previews: PreviewProvider {
+    static var previews: some View {
+        EnterNameView()
+    }
+}
