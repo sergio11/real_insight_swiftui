1860109bcabd562ff40ca0fba83419320b894d4d
Sergio Sánchez <sss4esob@gmail.com>
2024-02-29 19:34:49 +0100

    Implement MainView

---
+++ RealInsight/RealInsight/MainView.swift
@@ -0,0 +1,30 @@
+//
+//  MainView.swift
+//  RealInsight
+//
+//  Created by Sergio Sánchez Sánchez on 29/2/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    @EnvironmentObject var viewModel: AuthenticationViewModel
+    
+    var body: some View {
+        Group {
+            if viewModel.userSession == nil {
+                MainAuthenticationView()
+                    .environmentObject(viewModel)
+            } else {
+                ContentView()
+            }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ RealInsight/RealInsight/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  RealInsight
+//
+//  Created by Sergio Sánchez Sánchez on 29/2/24.
+//
+
+import Firebase
+import FirebaseFirestoreSwift
+
+struct User: Decodable, Identifiable {
+    @DocumentID var id: String?
+    var username: String?
+    var profileImageUrl: String?
+    var namee: String
+    var date: String
+    var bio: String?
+    var location: String?
+}
