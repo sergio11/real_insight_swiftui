4ec57eb8894586a6487d2d5d47912277938f7d9e
Sergio Sánchez <sss4esob@gmail.com>
2024-03-01 20:53:12 +0100

    Implement CameraView

--- RealInsight/RealInsight/CameraView.swift
+++ RealInsight/RealInsight/CameraView.swift
@@ -10,6 +10,13 @@ import SwiftUI
 struct CameraView: View {
     
     @State private var switchingCamera: Bool = false
+    @State private var takePhotoClicked: Bool = false
+    @State private var selectedBackImage: UIImage?
+    @State private var selectedFrontImage: UIImage?
+    @State private var backImage: Image?
+    @State private var frontImage: Image?
+    @State private var choseFromFront: Bool = false
+    @State private var photoTaken: Bool = false
     
     var body: some View {
         VStack {
@@ -33,40 +40,129 @@ struct CameraView: View {
                         .font(.system(size: 20))
                         .fontWeight(.heavy)
                     
-                    RoundedRectangle(cornerRadius: 12)
-                        .foregroundColor(.gray)
-                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.6)
-                        .overlay(
+                    ZStack(alignment: .topLeading) {
+                        GeometryReader { g in
                             VStack {
-                                ProgressView()
-                                Text("Wait, wait, wait, now smile")
+                                if frontImage != nil {
+                                    HStack {
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .frame(width: 125, height: 165)
+                                            .overlay(
+                                                VStack {
+                                                    if let imageFront = frontImage {
+                                                        imageFront.resizable()
+                                                            .cornerRadius(8)
+                                                            .frame(width: 120, height: 160)
+                                                            .scaledToFit()
+                                                    }
+                                                }
+                                            )
+                                            .padding(.leading)
+                                        Spacer()
+                                    }
+                                    .padding(.top, 18)
+                                }
+                                Spacer()
+                            }.sheet(isPresented: $choseFromFront) {
+                                onLoadFrontImage()
+                                self.photoTaken.toggle()
+                                self.switchingCamera.toggle()
+                            } content: {
+                                ImagePicker(image: $selectedFrontImage)
                             }
-                                .foregroundColor(.white)
-                                .opacity(self.switchingCamera ? 1 : 0)
-                        )
+                        }.zIndex(1)
+                        
+                        if let image = backImage {
+                            image.resizable()
+                                .cornerRadius(12)
+                                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.6)
+                                .scaledToFit()
+                                .overlay(
+                                    VStack {
+                                        ProgressView()
+                                        Text("Wait, wait, wait, now smile")
+                                    }
+                                        .foregroundColor(.white)
+                                        .opacity(self.switchingCamera ? 1 : 0)
+                                )
+                        } else {
+                            RoundedRectangle(cornerRadius: 12)
+                                .foregroundColor(.gray)
+                                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.6)
+                                .overlay(
+                                    VStack {
+                                        ProgressView()
+                                        Text("Wait, wait, wait, now smile")
+                                    }
+                                        .foregroundColor(.white)
+                                        .opacity(self.switchingCamera ? 1 : 0)
+                                ).sheet(isPresented: $takePhotoClicked){
+                                    onLoadBackImage()
+                                    self.switchingCamera.toggle()
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                                        self.choseFromFront.toggle()
+                                    }
+                                } content: {
+                                    ImagePicker(image: $selectedBackImage)
+                                }
+                        }
+                    }
+                    
                     
                     VStack {
-                        HStack(alignment: .center, spacing: 18) {
-                            Image(systemName: "bolt.slash.fill")
-                                .font(.system(size: 28))
-                            
+                        if photoTaken {
                             Button {
-                                
+                                onSend()
                             } label: {
-                                Image(systemName: "circle")
-                                    .font(.system(size: 70))
+                                HStack {
+                                    Text("SEND")
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 40, weight: .black))
+                                    Image(systemName: "location.north.fill")
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 20, weight: .black))
+                                        .rotationEffect(.degrees(90))
+                                }
                             }
-                            
-                            Image(systemName: "arrow.triangle.2.circlepath")
-                                .font(.system(size: 24))
-                        }.foregroundColor(.white)
+                        } else {
+                            HStack(alignment: .center, spacing: 18) {
+                                Image(systemName: "bolt.slash.fill")
+                                    .font(.system(size: 28))
+                                
+                                Button {
+                                    self.takePhotoClicked.toggle()
+                                } label: {
+                                    Image(systemName: "circle")
+                                        .font(.system(size: 70))
+                                }
+                                
+                                Image(systemName: "arrow.triangle.2.circlepath")
+                                    .font(.system(size: 24))
+                            }.foregroundColor(.white)
+                        }
                     }
+                    .offset(y: -20)
                     Spacer()
                 }
                 .padding(.top, 50)
             }
         }
     }
+    
+    private func onLoadBackImage() {
+        guard let selectedBackImage = selectedBackImage else { return }
+        backImage = Image(uiImage: selectedBackImage)
+    }
+    
+    private func onLoadFrontImage() {
+        guard let selectedFrontImage = selectedFrontImage else { return }
+        frontImage = Image(uiImage: selectedFrontImage)
+    }
+    
+    private func onSend() {
+        
+    }
+    
 }
 
 struct CameraView_Previews: PreviewProvider {
