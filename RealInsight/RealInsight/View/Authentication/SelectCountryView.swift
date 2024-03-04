ec63f170ba2442ac73831b0515d52a66f24c98e4
Sergio Sánchez <sss4esob@gmail.com>
2024-03-04 18:45:36 +0100

    Implement several improvements in SelectCountryView

--- RealInsight/RealInsight/View/Authentication/SelectCountryView.swift
+++ RealInsight/RealInsight/View/Authentication/SelectCountryView.swift
@@ -9,68 +9,86 @@ import SwiftUI
 
 struct SelectCountryView: View {
     
-    var countries: [Country] = Country.allCountries
-    
     @Binding var countryChosen: Country
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Color.black.ignoresSafeArea()
+                TopBar()
+                CountryList(countryChosen: $countryChosen)
+            }
+            .environment(\.colorScheme, .dark)
+        }
+    }
+}
+
+private struct TopBar: View {
     
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
         VStack {
             ZStack {
-                Color.black.ignoresSafeArea()
-                VStack {
-                    ZStack {
-                        Text("Select Country")
+                Text("Select Country")
+                    .foregroundColor(.white)
+                    .fontWeight(.semibold)
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
+                            .font(.system(size: 16))
                             .foregroundColor(.white)
                             .fontWeight(.semibold)
-                        HStack {
-                            Button {
-                                dismiss()
-                            } label: {
-                                Image(systemName: "xmark")
-                                    .font(.system(size: 16))
-                                    .foregroundColor(.white)
-                                    .fontWeight(.semibold)
-                            }
-                            Spacer()
-                        }
                     }
                     Spacer()
                 }
-                .padding(.horizontal)
-                
-                VStack {
-                    VStack {
-                        List {
-                            Section {
-                                ForEach(countries, id: \.isoCode) { country in
-                                    HStack {
-                                        Text("\(country.flag(country: country.isoCode)) \(country.localizedName) (+\(country.phoneCode))")
-                                        
-                                        Spacer()
-                                        
-                                        if country.isoCode == countryChosen.isoCode {
-                                            Image(systemName: "checkmark.circle")
-                                        }
-                                    }
-                                    .onTapGesture {
-                                        countryChosen = country
-                                        dismiss()
-                                    }
+            }
+            Spacer()
+        }
+        .padding(.horizontal)
+    }
+}
+
+
+private struct CountryList: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @Binding var countryChosen: Country
+    
+    var countries: [Country] = Country.allCountries
+    
+    var body: some View {
+        VStack {
+            VStack {
+                List {
+                    Section {
+                        ForEach(countries, id: \.isoCode) { country in
+                            HStack {
+                                Text("\(country.flag(country: country.isoCode)) \(country.localizedName) (+\(country.phoneCode))")
+                                
+                                Spacer()
+                                
+                                if country.isoCode == countryChosen.isoCode {
+                                    Image(systemName: "checkmark.circle")
                                 }
-                            } header: {
-                                 Text("Suggested")
-                                    .padding(.leading, -8)
-                                    .font(.system(size: 12))
+                            }
+                            .onTapGesture {
+                                countryChosen = country
+                                dismiss()
                             }
                         }
+                    } header: {
+                         Text("Suggested")
+                            .padding(.leading, -8)
+                            .font(.system(size: 12))
                     }
                 }
-                .padding(.top, 50)
             }
-            .environment(\.colorScheme, .dark)
         }
+        .padding(.top, 50)
     }
 }
 
