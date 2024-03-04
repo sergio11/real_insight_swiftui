e360db7de8a2942ff4b341f0f480e862dc72122f
Sergio Sánchez <sss4esob@gmail.com>
2024-03-04 21:10:06 +0100

    Implement several improvements in Settings Help View

--- RealInsight/RealInsight/View/Settings/HelpView.swift
+++ RealInsight/RealInsight/View/Settings/HelpView.swift
@@ -8,61 +8,77 @@
 import SwiftUI
 
 struct HelpView: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                Color.black.ignoresSafeArea()
+                TopBarView()
+                VStack {
+                    MainMenuOptions()
+                }
+            }
+        }
+    }
+}
+
+private struct TopBarView: View {
     
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
         VStack {
             ZStack {
-                Color.black.ignoresSafeArea()
-                
-                VStack {
-                    ZStack {
-                        Text("Help")
+                Text("Help")
+                    .foregroundColor(.white)
+                    .fontWeight(.semibold)
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "arrow.backward")
                             .foregroundColor(.white)
-                            .fontWeight(.semibold)
-                        
-                        HStack {
-                            Button {
-                                dismiss()
-                            } label: {
-                                Image(systemName: "arrow.backward")
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 20))
-                            }
-                            Spacer()
-                        }
+                            .font(.system(size: 20))
                     }
-                    
                     Spacer()
-                }
-                
-                VStack {
-                    
-                    VStack(spacing: 16) {
-                        
-                        NavigationLink {
-                            ContactUsView().navigationBarBackButtonHidden()
-                        } label: {
-                            ChevronButtonView(icon: "envelop", text: "Contact us")
-                        }
-                        
-                        NavigationLink {
-                            
-                        } label: {
-                            ChevronButtonView(icon: "questionmark.circle", text: "Help Center")
-                        }
-                        
-                        Spacer()
-                    }
-                    .padding(.horizontal)
-                    .padding(.top, 50)
-                    
-                }
+                }.padding(.horizontal)
             }
+            Spacer()
         }
     }
 }
+
+private struct MainMenuOptions: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            ContactUsButton()
+            HelpButton()
+            Spacer()
+        }
+        .padding(.horizontal)
+        .padding(.top, 50)
+    }
+}
+
+private struct ContactUsButton: View {
+    var body: some View {
+        NavigationLink {
+            ContactUsView().navigationBarBackButtonHidden()
+        } label: {
+            ChevronButtonView(icon: "envelope", text: "Contact us")
+        }
+    }
+}
+
+private struct HelpButton: View {
+    var body: some View {
+        NavigationLink {
+            
+        } label: {
+            ChevronButtonView(icon: "questionmark.circle", text: "Help Center")
+        }
+    }
+}
+
 
 struct HelpView_Previews: PreviewProvider {
     static var previews: some View {
