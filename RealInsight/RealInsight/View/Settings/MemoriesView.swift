dd5c23029aad0a792fd1b6ee240c1fd2043b4dff
Sergio Sánchez <sss4esob@gmail.com>
2024-03-04 20:58:56 +0100

    Implement several improvements in MemoriesView

--- RealInsight/RealInsight/View/Settings/MemoriesView.swift
+++ RealInsight/RealInsight/View/Settings/MemoriesView.swift
@@ -15,106 +15,135 @@ struct MemoriesView: View {
         VStack {
             ZStack {
                 Color.black.ignoresSafeArea()
+                TopBarView()
                 VStack {
-                    ZStack {
-                        Text("Memories")
+                    DeactivateMemoriesTitle()
+                    DeactivateMemoriesBox()
+                    Spacer()
+                }
+                .padding(.horizontal)
+                .padding(.top, 50)
+            }
+        }
+    }
+}
+
+private struct TopBarView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Text("Memories")
+                    .foregroundColor(.white)
+                    .fontWeight(.semibold)
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "arrow.backward")
                             .foregroundColor(.white)
-                            .fontWeight(.semibold)
-                        
-                        HStack {
-                            
-                            Button {
-                                dismiss()
-                            } label: {
-                                Image(systemName: "arrow.backward")
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 20))
-                            }
-                            
-                            Spacer()
-                            
-                            Image(systemName: "questionmark.circle")
-                                .foregroundColor(.white)
-                                .font(.system(size: 16))
-                            
-                        }
+                            .font(.system(size: 20))
                     }
+                    Spacer()
+                    Image(systemName: "questionmark.circle")
+                        .foregroundColor(.white)
+                        .font(.system(size: 16))
+                }.padding(.horizontal)
+            }
+            Spacer()
+        }
+    }
+}
+
+private struct MainTitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Your memories are activated")
+                    .foregroundColor(.white)
+                    .fontWeight(.semibold)
+                    .font(.system(size: 20))
+                Spacer()
+            }
+            Text("All your RealInsight are automatically added to your Memories and only visible by you")
+                .foregroundColor(.white)
+                .padding(.top, -2)
+        }
+    }
+}
+
+
+private struct DeactivateMemoriesTitle: View {
+    var body: some View {
+        HStack {
+            Text("Deactivate and Delete Memories")
+                .foregroundColor(.white)
+                .font(.system(size: 20))
+                .fontWeight(.bold)
+            
+            Spacer()
+        }
+    }
+}
+
+private struct DeactivateMemoriesBox: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .frame(height: 210)
+                .foregroundColor(Color(red: 22/255, green: 4/255, blue: 3/255))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16).stroke(.red, lineWidth: 1)
+                )
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Deactivate and Delete Memories")
+                        .foregroundColor(.white)
+                        .font(.system(size: 20))
+                        .fontWeight(.bold)
                     
                     Spacer()
                 }
-                
                 VStack {
-                    VStack {
-                        HStack {
-                            Text("Your memories are activated")
-                                .foregroundColor(.white)
-                                .fontWeight(.semibold)
-                                .font(.system(size: 20))
-                            Spacer()
-                        }
-                        Text("All your RealInsight are automatically added to your Memories and only visible by you")
+                    HStack {
+                        Text("If you deactivate your Memories, all your RealInsights will be permanently deleted and unrecoverable")
                             .foregroundColor(.white)
-                            .padding(.top, -2)
+                            .font(.system(size: 14))
+                        Spacer()
                     }
-                    
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 25)
-                            .frame(height: 210)
-                            .foregroundColor(Color(red: 22/255, green: 4/255, blue: 3/255))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 16).stroke(.red, lineWidth: 1)
-                            )
-                        
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Text("Deactivate and Delete Memories")
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 20))
-                                    .fontWeight(.bold)
-                                
-                                Spacer()
-                            }
-                            
-                            VStack {
-                                HStack {
-                                    Text("If you deactivate your Memories, all your RealInsights will be permanently deleted and unrecoverable")
-                                        .foregroundColor(.white)
-                                        .font(.system(size: 14))
-                                    Spacer()
-                                }
-                                HStack {
-                                    Text("All your future RealInsights won't be saved in Memories and will be automatically deleted as well.")
-                                        .foregroundColor(.white)
-                                        .font(.system(size: 14))
-                                    Spacer()
-                                }
-                            }
-                            .padding(.top, -6)
-                            
-                            
-                            RoundedRectangle(cornerRadius: 12)
-                                .frame(width: UIScreen.main.bounds.width * 0.5, height: 40)
-                                .foregroundColor(Color(red: 44/255, green: 44/255, blue: 46/255))
-                                .overlay(
-                                    Text("Deactivate Memories")
-                                        .foregroundColor(.red)
-                                        .font(.system(size: 15))
-                                        .fontWeight(.semibold)
-                                )
-                                .padding(.top, 8)
-                            
-                            
-                        }.padding(.leading)
+                    HStack {
+                        Text("All your future RealInsights won't be saved in Memories and will be automatically deleted as well.")
+                            .foregroundColor(.white)
+                            .font(.system(size: 14))
+                        Spacer()
                     }
-                    .padding(.top, 22)
-                    Spacer()
-                    
-                    
-                }.padding(.horizontal)
-                    .padding(.top, 50)
+                }
+                .padding(.top, -6)
                 
-            }
+                
+                DeactivateMemoriesButton()
+                
+            }.padding(.leading)
         }
+        .padding(.top, 22)
+    }
+}
+
+private struct DeactivateMemoriesButton: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 12)
+            .frame(width: UIScreen.main.bounds.width * 0.5, height: 40)
+            .foregroundColor(Color(red: 44/255, green: 44/255, blue: 46/255))
+            .overlay(
+                Text("Deactivate Memories")
+                    .foregroundColor(.red)
+                    .font(.system(size: 15))
+                    .fontWeight(.semibold)
+            )
+            .padding(.top, 8)
     }
 }
 
