c35181768a0013e7cf39cec09aecf664c7ef131f
Sergio Sánchez <sss4esob@gmail.com>
2024-03-04 21:45:42 +0100

    Implement several improvements in TimeZoneView

--- RealInsight/RealInsight/View/Settings/TimeZoneView.swift
+++ RealInsight/RealInsight/View/Settings/TimeZoneView.swift
@@ -8,225 +8,177 @@
 import SwiftUI
 
 struct TimeZoneView: View {
-    
-    @Environment(\.dismiss) var dismiss
     @State var area = "europe"
     
     var body: some View {
         VStack {
             ZStack {
                 Color.black.ignoresSafeArea()
+                TopBarView()
                 VStack {
-                    ZStack {
-                        Text("Time Zone")
-                            .foregroundColor(.white)
-                            .fontWeight(.semibold)
-                        HStack {
-                            Button {
-                                dismiss()
-                            } label: {
-                                Image(systemName: "arrow.backward")
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 20))
-                            }
-                            Spacer()
-                        }
-                    }
-                    .padding(.horizontal)
-                    Spacer()
-                }
-                
-                
-                VStack {
-                    
                     VStack {
-                        HStack {
-                            Text("Select your Time zone")
-                                .foregroundColor(.white)
-                                .fontWeight(.bold)
-                                .font(.system(size: 20))
-                            
-                            Spacer()
-                        }
-                        
-                        HStack {
-                            Text("To receive your RealInsight notification during daytime select your time zone. When changing your time zone, your current RealInsight will be deleted. You can only change time zones once a day")
-                                .foregroundColor(.white)
-                                .font(.system(size: 14))
-                                .fontWeight(.semibold)
-                            Spacer()
-                        }
-                        .padding(.top, -8)
-                        
+                        MainTitleTextView()
+                        MainDescriptionTextView()
                     }
                     
-                    
-                    VStack {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 14)
-                                .frame(width: UIScreen.main.bounds.width * 0.9, height: 190)
-                                .foregroundColor(.white)
-                                .opacity(0.07)
-                            
-                            VStack {
-                                Button {
-                                    self.area = "europe"
-                                } label: {
-                                    HStack {
-                                        Image(systemName: "globe.europe.africa.fill")
-                                            .foregroundColor(.white)
-                                        
-                                        Text("Europe")
-                                            .foregroundColor(.white)
-                                            .fontWeight(.semibold)
-                                        
-                                        Spacer()
-                                        
-                                        if self.area == "europe" {
-                                            Image(systemName: "checkmark.circle")
-                                                .foregroundColor(.gray)
-                                                .font(.system(size: 14))
-                                        }
-                                        
-                                    }
-                                    .padding(.horizontal, UIScreen.main.bounds.width * 0.05)
-                                    .frame(height: 30)
-                                }
-                                
-                                HStack {
-                                    Spacer()
-                                    Rectangle()
-                                        .frame(width: UIScreen.main.bounds.width * 0.8, height: 0.3)
-                                        .opacity(0.4)
-                                        .foregroundColor(.gray)
-                                }
-                                
-                                Button {
-                                    self.area = "americas"
-                                } label: {
-                                    HStack {
-                                        Image(systemName: "globe.europe.africa.fill")
-                                            .foregroundColor(.white)
-                                        
-                                        Text("Americas")
-                                            .foregroundColor(.white)
-                                            .fontWeight(.semibold)
-                                        
-                                        Spacer()
-                                        
-                                        if self.area == "americas" {
-                                            Image(systemName: "checkmark.circle")
-                                                .foregroundColor(.gray)
-                                                .font(.system(size: 14))
-                                        }
-                                        
-                                    }
-                                    .padding(.horizontal, UIScreen.main.bounds.width * 0.05)
-                                    .frame(height: 30)
-                                }
-                                
-                                HStack {
-                                    Spacer()
-                                    Rectangle()
-                                        .frame(width: UIScreen.main.bounds.width * 0.8, height: 0.3)
-                                        .opacity(0.4)
-                                        .foregroundColor(.gray)
-                                }
-                                
-                                
-                                Button {
-                                    self.area = "eastasia"
-                                } label: {
-                                    HStack {
-                                        Image(systemName: "globe.europe.africa.fill")
-                                            .foregroundColor(.white)
-                                        
-                                        Text("East Asia")
-                                            .foregroundColor(.white)
-                                            .fontWeight(.semibold)
-                                        
-                                        Spacer()
-                                        
-                                        if self.area == "eastasia" {
-                                            Image(systemName: "checkmark.circle")
-                                                .foregroundColor(.gray)
-                                                .font(.system(size: 14))
-                                        }
-                                        
-                                    }
-                                    .padding(.horizontal, UIScreen.main.bounds.width * 0.05)
-                                    .frame(height: 30)
-                                }
-                                
-                                HStack {
-                                    Spacer()
-                                    Rectangle()
-                                        .frame(width: UIScreen.main.bounds.width * 0.8, height: 0.3)
-                                        .opacity(0.4)
-                                        .foregroundColor(.gray)
-                                }
-                                
-                                Button {
-                                    self.area = "westasia"
-                                } label: {
-                                    HStack {
-                                        Image(systemName: "globe.europe.africa.fill")
-                                            .foregroundColor(.white)
-                                        
-                                        Text("West Asia")
-                                            .foregroundColor(.white)
-                                            .fontWeight(.semibold)
-                                        
-                                        Spacer()
-                                        
-                                        if self.area == "westasia" {
-                                            Image(systemName: "checkmark.circle")
-                                                .foregroundColor(.gray)
-                                                .font(.system(size: 14))
-                                        }
-                                        
-                                    }
-                                    .padding(.horizontal, UIScreen.main.bounds.width * 0.05)
-                                    .frame(height: 30)
-                                }
-                                
-                                HStack {
-                                    Spacer()
-                                    Rectangle()
-                                        .frame(width: UIScreen.main.bounds.width * 0.8, height: 0.3)
-                                        .opacity(0.4)
-                                        .foregroundColor(.gray)
-                                }
-                                
-                                
-                            }
-                            
-                        }
-                        .padding(.top)
-                    }
-                    
-                    
+                    MainMenuOptions(selectedArea: $area)
                     Spacer()
-                    
-                    Button {} label: {
-                        RoundedRectangle(cornerRadius: 14)
-                            .frame(width: UIScreen.main.bounds.width * 0.9, height: 45)
-                            .foregroundColor(Color(red: 86/255, green: 86/255, blue: 88/255))
-                            .overlay(
-                                Text("Save")
-                                    .foregroundColor(.black)
-                                    .font(.system(size: 14))
-                            )
-                    }
-                    
+                    MainButton()
                 }
                 .padding(.top, 50)
+                .padding(.bottom, 40)
                 .padding(.horizontal)
-                
-                
             }
+        }
+    }
+}
+
+private struct TopBarView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Text("Time Zone")
+                    .foregroundColor(.white)
+                    .fontWeight(.semibold)
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "arrow.backward")
+                            .foregroundColor(.white)
+                            .font(.system(size: 20))
+                    }
+                    Spacer()
+                }
+            }
+            .padding(.horizontal)
+            Spacer()
+        }
+    }
+}
+
+private struct MainTitleTextView: View {
+    var body: some View {
+        HStack {
+            Text("Select your Time zone")
+                .foregroundColor(.white)
+                .fontWeight(.bold)
+                .font(.system(size: 20))
+            Spacer()
+        }
+    }
+}
+
+private struct MainDescriptionTextView: View {
+    var body: some View {
+        HStack {
+            Text("To receive your RealInsight notification during daytime select your time zone. When changing your time zone, your current RealInsight will be deleted. You can only change time zones once a day")
+                .foregroundColor(.white)
+                .font(.system(size: 14))
+                .fontWeight(.semibold)
+            Spacer()
+        }
+        .padding(.top, -8)
+    }
+}
+
+
+private struct MainMenuOptions: View {
+    
+    @Binding var selectedArea: String
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 14)
+                    .frame(width: UIScreen.main.bounds.width * 0.9, height: 190)
+                    .foregroundColor(.white)
+                    .opacity(0.07)
+                
+                VStack {
+                    AreaSelectionButton(
+                        areaName: "europe",
+                        areaDescription: "Europe",
+                        selectedArea: $selectedArea
+                    )
+                    AreaSelectionButton(
+                        areaName: "americas",
+                        areaDescription: "Americas",
+                        selectedArea: $selectedArea
+                    )
+                    AreaSelectionButton(
+                        areaName: "eastasia",
+                        areaDescription: "East Asia",
+                        selectedArea: $selectedArea
+                    )
+                    AreaSelectionButton(
+                        areaName: "westasia",
+                        areaDescription: "West Asia",
+                        selectedArea: $selectedArea
+                    )
+                }
+            }
+            .padding(.top)
+        }
         
-        
+    }
+}
+
+private struct MainButton: View {
+    var body: some View {
+        Button {
+            
+        } label: {
+            RoundedRectangle(cornerRadius: 14)
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: 45)
+                .foregroundColor(Color(red: 86/255, green: 86/255, blue: 88/255))
+                .overlay(
+                    Text("Save")
+                        .foregroundColor(.black)
+                        .font(.system(size: 14))
+                )
+        }
+    }
+}
+
+private struct AreaSelectionButton: View {
+    
+    var areaName: String
+    var areaDescription: String
+    @Binding var selectedArea: String
+    
+    var body: some View {
+        Button {
+            selectedArea = areaName
+        } label: {
+            HStack {
+                Image(systemName: "globe.europe.africa.fill")
+                    .foregroundColor(.white)
+                
+                Text(areaDescription)
+                    .foregroundColor(.white)
+                    .fontWeight(.semibold)
+                
+                Spacer()
+                
+                if selectedArea == areaName {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.gray)
+                        .font(.system(size: 14))
+                }
+            }
+            .padding(.horizontal, UIScreen.main.bounds.width * 0.05)
+            .frame(height: 30)
+        }
+        HStack {
+            Spacer()
+            Rectangle()
+                .frame(width: UIScreen.main.bounds.width * 0.8, height: 0.3)
+                .opacity(0.4)
+                .foregroundColor(.gray)
         }
     }
 }
