2a8223d2cb5f8264ee318f54abfb30ca3543d320
Sergio Sánchez <sss4esob@gmail.com>
2024-03-05 21:48:09 +0100

    Implement several improvements in ProfileView

--- RealInsight/RealInsight/View/ProfileView.swift
+++ RealInsight/RealInsight/View/ProfileView.swift
@@ -18,149 +18,163 @@ struct ProfileView: View {
         VStack {
             ZStack {
                 Color.black.ignoresSafeArea()
-                
+                TopBarView(mainMenu: $mainMenu)
                 VStack {
-                    HStack {
-                        
-                        Button {
-                            withAnimation {
-                                self.mainMenu = "feed"
-                            }
-                        } label: {
-                            Image(systemName: "arrow.backward")
-                                .foregroundColor(.white)
-                                .font(.system(size: 20))
-                        }
-                        Spacer()
-                        Text("Profile")
-                            .foregroundColor(.white)
-                            .fontWeight(.semibold)
-                        Spacer()
-                        
-                        NavigationLink {
-                            SettingsView().navigationBarBackButtonHidden()
-                        } label:  {
-                            ThreeDots(size: 4, color: .white)
-                        }
-                        
-                    }
-                    .padding(.horizontal)
-                    Spacer()
-                }
-                
-                VStack {
-                    
-                    if let profileImageUrl = viewModel.currentUser?.profileImageUrl {
-                        KFImage(URL(string: profileImageUrl))
-                            .resizable()
-                            .frame(width: 130, height: 130)
-                            .cornerRadius(75)
-                    } else {
-                        Circle()
-                            .frame(width: 130, height: 130)
-                            .cornerRadius(75)
-                            .foregroundColor(Color(red: 152/255, green: 163/255, blue: 16/255))
-                            .overlay(
-                                Text(viewModel.currentUser!.fullname.prefix(1).uppercased())
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 55))
-                            )
-                    }
-                    
-                    Text(viewModel.fullName)
-                        .foregroundColor(.white)
-                        .font(.system(size: 25))
-                        .fontWeight(.bold)
-                    
-                    Text(viewModel.username)
-                        .foregroundColor(.white)
-                        .fontWeight(.semibold)
-                    
-                    HStack {
-                        Text("Your memories")
-                            .foregroundColor(.white)
-                            .fontWeight(.semibold)
-                            .font(.system(size: 20))
-                        
-                        Spacer()
-                        HStack(spacing: 4) {
-                            Image(systemName: "lock.fill")
-                                .foregroundColor(.gray)
-                                .font(.system(size: 10))
-                            
-                            Text("Only visible to you")
-                                .foregroundColor(.gray)
-                                .font(.system(size: 10))
-                            
-                        }
-                        
-                    }
-                    .padding(.horizontal)
-                    .padding(.top, 4)
-                    
+                    ProfileInfoView()
+                    YourMemoriesView()
                     VStack {
                         ZStack {
                             RoundedRectangle(cornerRadius: 16)
                                 .foregroundColor(.white)
                                 .opacity(0.07)
                                 .frame(height: 230)
-                            
                             VStack {
-                                HStack {
-                                    Text("Last 14 days")
-                                        .foregroundColor(.white)
-                                        .font(.system(size: 16))
-                                        .padding(.top, 8)
-                                    Spacer()
-                                }
-                                .padding(.leading)
-                                
-                                VStack {
-                                    HStack(spacing: 4) {
-                                        
-                                        ForEach(1..<8) { num in
-                                            MemoryView(day: num)
-                                        }
-                                        
-                                    }
-                                    
-                                    HStack(spacing: 4) {
-                                        
-                                        ForEach(1..<8) { num in
-                                            MemoryView(day: num+7)
-                                        }
-                                        
-                                    }
-                                    .padding(.top, -4)
-                                }
-                                .padding(.top, -4)
-                                
-                                Text("View all my memories")
-                                    .foregroundColor(.white)
-                                    .fontWeight(.semibold)
-                                    .font(.system(size: 13))
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 8)
-                                            .stroke(.gray, lineWidth: 2)
-                                            .frame(width: 175, height: 20)
-                                            .opacity(0.5)
-                                    )
-                                    .padding(.top, 4)
+                                ProfileMemoriesTitleView()
+                                ProfileMemoriesView()
+                                ViewAllMemoriesButton()
                             }
                             .padding(.top, -15)
                         }
                     }
-                    
-                    Text("Real/dreamsoftware")
-                        .foregroundColor(.white)
-                        .fontWeight(.semibold)
-                        .font(.system(size: 16))
-                        .padding(.top, 8)
-                    
+                    UserLinkButton()
                     Spacer()
                 }.padding(.top, 35)
             }
         }
+    }
+}
+
+private struct TopBarView: View {
+    
+    @Binding var mainMenu: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    withAnimation{
+                        self.mainMenu = "feed"
+                    }
+                } label: {
+                    Image(systemName: "arrow.backward")
+                        .foregroundColor(.white)
+                        .font(.system(size: 20))
+                }
+                Spacer()
+                Text("Profile")
+                    .foregroundColor(.white)
+                    .fontWeight(.semibold)
+                Spacer()
+                NavigationLink {
+                    SettingsView().navigationBarBackButtonHidden()
+                } label:  {
+                    ThreeDots(size: 4, color: .white)
+                }
+            }
+            .padding(.horizontal)
+            Spacer()
+        }
+    }
+}
+
+private struct ProfileInfoView: View {
+    
+    @EnvironmentObject var viewModel: AuthenticationViewModel
+    
+    var body: some View {
+        ProfileImageView(
+            size: 130,
+            cornerRadius: 75,
+            profileImageUrl: viewModel.currentUser?.profileImageUrl,
+            fullName: viewModel.fullName
+        )
+        Text(viewModel.fullName)
+            .foregroundColor(.white)
+            .font(.system(size: 25))
+            .fontWeight(.bold)
+        Text(viewModel.username)
+            .foregroundColor(.white)
+            .fontWeight(.semibold)
+    }
+}
+
+private struct ProfileMemoriesTitleView: View {
+    var body: some View {
+        HStack {
+            Text("Last 14 days")
+                .foregroundColor(.white)
+                .font(.system(size: 16))
+                .padding(.top, 8)
+            Spacer()
+        }
+        .padding(.leading)
+    }
+}
+
+private struct ProfileMemoriesView: View {
+    var body: some View {
+        VStack {
+            HStack(spacing: 4) {
+                ForEach(1..<8) { num in
+                    MemoryView(day: num)
+                }
+            }
+            HStack(spacing: 4) {
+                ForEach(1..<8) { num in
+                    MemoryView(day: num+7)
+                }
+            }
+            .padding(.top, -4)
+        }
+        .padding(.top, -4)
+    }
+}
+
+private struct ViewAllMemoriesButton: View {
+    var body: some View {
+        Text("View all my memories")
+            .foregroundColor(.white)
+            .fontWeight(.semibold)
+            .font(.system(size: 13))
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(.gray, lineWidth: 2)
+                    .frame(width: 175, height: 20)
+                    .opacity(0.5)
+            )
+            .padding(.top, 4)
+    }
+}
+
+private struct UserLinkButton: View {
+    var body: some View {
+        Text("Real/dreamsoftware")
+            .foregroundColor(.white)
+            .fontWeight(.semibold)
+            .font(.system(size: 16))
+            .padding(.top, 8)
+    }
+}
+
+private struct YourMemoriesView: View {
+    var body: some View {
+        HStack {
+            Text("Your memories")
+                .foregroundColor(.white)
+                .fontWeight(.semibold)
+                .font(.system(size: 20))
+            Spacer()
+            HStack(spacing: 4) {
+                Image(systemName: "lock.fill")
+                    .foregroundColor(.gray)
+                    .font(.system(size: 10))
+                Text("Only visible to you")
+                    .foregroundColor(.gray)
+                    .font(.system(size: 10))
+            }
+        }
+        .padding(.horizontal)
+        .padding(.top, 4)
     }
 }
 
