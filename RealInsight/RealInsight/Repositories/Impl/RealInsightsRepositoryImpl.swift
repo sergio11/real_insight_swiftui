d819611137f69a015ba64bef548b9e461c9f2a8b
Sergio Sánchez <sss4esob@gmail.com>
2024-04-04 20:21:51 +0200

    Fetching real insights from friends

--- RealInsight/RealInsight/Repositories/Impl/RealInsightsRepositoryImpl.swift
+++ RealInsight/RealInsight/Repositories/Impl/RealInsightsRepositoryImpl.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Firebase
 
+/// An implementation of the `RealInsightsRepository` protocol responsible for managing real insights data.
 internal class RealInsightsRepositoryImpl: RealInsightsRepository {
     
     private let realInsightsDataSource: RealInsightsDataSource
@@ -15,6 +16,12 @@ internal class RealInsightsRepositoryImpl: RealInsightsRepository {
     private let storageFilesDataSource: StorageFilesDataSource
     private let realInsightMapper: RealInsightMapper
     
+    /// Initializes a new instance of `RealInsightsRepositoryImpl`.
+        /// - Parameters:
+        ///   - realInsightsDataSource: The data source responsible for real insights operations.
+        ///   - userDataSource: The data source responsible for user operations.
+        ///   - storageFilesDataSource: The data source responsible for storage file operations.
+        ///   - realInsightMapper: The mapper responsible for mapping real insight data.
     init(realInsightsDataSource: RealInsightsDataSource, userDataSource: UserDataSource, storageFilesDataSource: StorageFilesDataSource, realInsightMapper: RealInsightMapper) {
         self.realInsightsDataSource = realInsightsDataSource
         self.userDataSource = userDataSource
@@ -22,9 +29,16 @@ internal class RealInsightsRepositoryImpl: RealInsightsRepository {
         self.realInsightMapper = realInsightMapper
     }
 
-    func fetchAllRealInsights(date: Date) async throws -> [RealInsight] {
+    /// Fetches all real insights for the specified date and user ID asynchronously.
+        /// - Parameters:
+        ///   - date: The date for which to fetch real insights.
+        ///   - userId: The ID of the user for which to fetch real insights.
+        /// - Returns: An array of `RealInsight` representing the fetched real insights.
+        /// - Throws: An error if the operation fails, including `realInsightNotFound` if no real insights are found.
+    func fetchAllRealInsights(date: Date, userId: String) async throws -> [RealInsight] {
         do {
-            let realInsightsData = try await realInsightsDataSource.fetchAllRealInsights(forDate: date)
+            let userData = try await userDataSource.getUserById(userId: userId)
+            let realInsightsData = try await realInsightsDataSource.fetchAllRealInsights(forDate: date, userIds: userData.friends)
             var realInsights: [RealInsight] = []
             for realInsightData in realInsightsData {
                 let user = try await userDataSource.getUserById(userId: realInsightData.userId)
@@ -38,6 +52,12 @@ internal class RealInsightsRepositoryImpl: RealInsightsRepository {
         }
     }
         
+    /// Fetches own real insights for the specified user and last number of days asynchronously.
+        /// - Parameters:
+        ///   - days: The number of past days from which to fetch real insights.
+        ///   - userId: The ID of the user for which to fetch real insights.
+        /// - Returns: An array of `RealInsight` representing the fetched real insights.
+        /// - Throws: An error if the operation fails, including `realInsightNotFound` if no real insights are found.
     func fetchOwnRealInsight(lastDays days: Int, userId: String) async throws -> [RealInsight] {
         do {
             let ownRealInsightList = try await realInsightsDataSource.fetchOwnRealInsight(lastDays: days, userId: userId)
@@ -53,6 +73,13 @@ internal class RealInsightsRepositoryImpl: RealInsightsRepository {
         }
     }
     
+    /// Posts a new real insight asynchronously.
+        /// - Parameters:
+        ///   - userId: The ID of the user posting the real insight.
+        ///   - backImageData: The data of the image taken from the back camera.
+        ///   - frontImageData: The data of the image taken from the front camera.
+        /// - Returns: A `RealInsight` object representing the posted real insight.
+        /// - Throws: An error if the operation fails, including `postFailed` if posting the real insight fails.
     func postRealInsight(userId: String, backImageData: Data, frontImageData: Data) async throws -> RealInsight {
         do {
             let backImageUrl = try await storageFilesDataSource.uploadImage(imageData: backImageData, type: .post)
--- RealInsight/RealInsight/Repositories/RealInsightsRepository.swift
+++ RealInsight/RealInsight/Repositories/RealInsightsRepository.swift
@@ -6,15 +6,39 @@
 //
 
 import Foundation
-import UIKit
 
+/// Enum representing errors that can occur during real insights repository operations.
 enum RealInsightsRepositoryError: Error {
+    /// Error indicating that posting a real insight failed.
     case postFailed
+    /// Error indicating that a real insight could not be found.
     case realInsightNotFound
 }
 
+/// A repository responsible for managing real insights data.
 protocol RealInsightsRepository {
-    func fetchAllRealInsights(date: Date) async throws -> [RealInsight]
+    /// Fetches all real insights for the specified date and user ID asynchronously.
+    /// - Parameters:
+    ///   - date: The date for which to fetch real insights.
+    ///   - userId: The ID of the user for which to fetch real insights.
+    /// - Returns: An array of `RealInsight` representing the fetched real insights.
+    /// - Throws: An error if the operation fails, including `realInsightNotFound` if no real insights are found.
+    func fetchAllRealInsights(date: Date, userId: String) async throws -> [RealInsight]
+    
+    /// Fetches own real insights for the specified user and last number of days asynchronously.
+    /// - Parameters:
+    ///   - days: The number of past days from which to fetch real insights.
+    ///   - userId: The ID of the user for which to fetch real insights.
+    /// - Returns: An array of `RealInsight` representing the fetched real insights.
+    /// - Throws: An error if the operation fails, including `realInsightNotFound` if no real insights are found.
     func fetchOwnRealInsight(lastDays days: Int, userId: String) async throws -> [RealInsight]
+    
+    /// Posts a new real insight asynchronously.
+    /// - Parameters:
+    ///   - userId: The ID of the user posting the real insight.
+    ///   - backImageData: The data of the image taken from the back camera.
+    ///   - frontImageData: The data of the image taken from the front camera.
+    /// - Returns: A `RealInsight` object representing the posted real insight.
+    /// - Throws: An error if the operation fails, including `postFailed` if posting the real insight fails.
     func postRealInsight(userId: String, backImageData: Data, frontImageData: Data) async throws -> RealInsight
 }
